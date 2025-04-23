c34059069e3f288c3a31762df485ac6f4db33c95
bimatakutkecoa <bimanaderli@gmail.com>
2025-04-23 15:23:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "bima jelek kata aleta,  [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.header("a rafe dawgh")
-st. image("IMG_20250422_121644_814.jpg")
+st.image("IMG_20250422_121644_814.jpg")
