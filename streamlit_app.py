4a92eae4220723469d70435a257c6735d9e10b95
bimatakutkecoa <bimanaderli@gmail.com>
2025-04-23 15:06:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 welcome to barudawgh")
 st.write(
-    "bima jelek,  [docs.streamlit.io](https://docs.streamlit.io/)."
+    "bima jelek kata aleta,  [docs.streamlit.io](https://docs.streamlit.io/)."
 )
