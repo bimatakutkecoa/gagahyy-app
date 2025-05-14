6075d0b85a3e1bc7de29f727125e27ce80ae953e
bimatakutkecoa <bimanaderli@gmail.com>
2025-05-14 14:44:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.title("welcome to barudawgh")
 st.write(
     "welcome to bima web."
-)never thought I could come this far with you,
+)
 st.header("a rafe dawgh")
 st.image("IMG_20250422_121644_814.jpg")
 
 st.title("special day")
 st.write(
-    "never thought I could come this far with you."
+    "never thought I could come this far with you🙀."
